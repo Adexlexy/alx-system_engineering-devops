3e44e79a524c42c18c3a701158ee2957dd651b36
Adexlexy <oshewolo_martins@yahoo.com>
2023-04-25 01:45:39 -0700

    Puppet task

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#Create as file in tmp folder with puppet
+
+file { '/tmp/school':
+	content => 'I love Puppet'
+	mode    => '0744',
+	owner   => 'www-data',
+	group   => 'www-data',
+}
+	
+	
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+Configuration Management wfocus majorly on the use of ppet and puppet lint
